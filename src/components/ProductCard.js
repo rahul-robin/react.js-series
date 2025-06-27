ad7b4faaf754d841720759ceb57535ea600f02c3
rahul uddin robn <rahuluddinrobin286@gamil.com>
2025-06-27 16:32:00 +0600

    added button feature

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import { productList } from "../utis/constant";
 import Product from "./Product";
 
 export const ProductCard = () => {
+  const [listOfProducts, setListOfProducts] = useState(productList);
+
   return (
-    <div className="product-card">
-      {productList.map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
+    <div>
+      <div>
+        <button
+          onClick={() => {
+            const filteredProducts = listOfProducts.filter(
+              (product) => product.rating.rate >= 4
+            );
+            setListOfProducts(filteredProducts);
+          }}
+          style={{ marginTop: "10px" }}
+        >
+          Top rated products
+        </button>
+      </div>
+      <div className="product-card">
+        {listOfProducts.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
